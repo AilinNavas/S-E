72b0fc22c37be3bffb302345d88d5477f78827e7
AilinNavas <navasailin18@gmail.com>
2025-05-03 10:53:25 -0300

    revision de cambios joel

--- app/data/TrustData.js
+++ app/data/TrustData.js
@@ -1,7 +1,6 @@
 import { Award, Cpu, Heart, AlertCircle, Handshake, HandHeart, Clock , MessageCircle} from 'lucide-react';
 
 export const trustPatients = [
-<<<<<<< HEAD
     {
       icon: Award,
       title: "Experienced Specialist",
@@ -49,55 +48,7 @@ export const trustPatients = [
     },
    
   ];
-=======
-  {
-    icon: Award,
-    title: "Experienced & Caring Specialists",
-    description: "Our endodontists have years of experience in pain-free root canal therapy.",
-    benefits: ["Gentle procedures", "Proven success rates"],
-  },
-  {
-    icon: Cpu,
-    title: "Advanced Technology",
-    description: "Cutting-edge tools for precise diagnostics and treatments.",
-    benefits: ["Digital X-rays", "3D Imaging"],
-  },
-  {
-    icon: Heart,
-    title: "Comfort & Anxiety-Free Treatments",
-    description: "Focus on patient comfort with sedation options available.",
-    benefits: ["Sedation available", "Friendly staff"],
-  },
-  {
-    icon: AlertCircle,
-    title: "Same-Day Emergency Care",
-    description: "Immediate appointments for urgent dental issues.",
-    benefits: ["Fast attention", "Pain relief first"],
-  },
-];
 
-export const trustServices = [
-  {
-    icon: Cpu,
-    title: "State-of-the-Art Technology",
-    description: "Our practice utilizes Cone Beam CT imaging and microscopic endodontics for unparalleled precision and effectiveness in treatment.",
-    benefits: ["3D imaging for accurate diagnosis", "Microscopic precision", "Digital record-keeping"],
-  },
-  {
-    icon: HandHeart,
-    title: "Pain-Free, Patient-Centered Care",
-    description: "We prioritize your comfort with sedation options, gentle techniques, and a compassionate approach to endodontic care.",
-    benefits: ["Anesthesia for painless treatment", "Sedation options for anxious patients", "Team focused on your comfort"],
-  },
-  {
-    icon: Handshake,
-    title: "Seamless Doctor Communication",
-    description: "We maintain clear communication with referring dentists through secure referrals and detailed case updates.",
-    benefits: ["Comprehensive treatment reports", "Secure digital image sharing", "Easy online referral process"],
-  },
->>>>>>> fbfbfc910ba6e2866160f6eeb5cfd7d293e0e1ab
-
-];
 
 export const trustDoctors = [
   {
