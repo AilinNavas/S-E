67d69df99a07e6ff3ddbb94d584a46222d8f83f9
AilinNavas <navasailin18@gmai.com>
2025-05-05 18:51:08 -0300

    footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -89,7 +89,7 @@ const Footer = () => {
               <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
             </svg>
           </Link>
-          <Link href="#" aria-label="Instagram">
+          {/* <Link href="#" aria-label="Instagram">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -124,7 +124,7 @@ const Footer = () => {
               <path d="M12 8v8" />
               <path d="M8 12h8" />
             </svg>
-          </Link>
+          </Link> */}
         </div>
         <div className="mt-8 text-center text-sm text-white">
           <p>&copy; {new Date().getFullYear()} Southside Endodontics. All rights reserved.</p>
